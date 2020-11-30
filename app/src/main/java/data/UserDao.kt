05dac480dbca03945d52295a6497e490b9e6fc59
wedieu <wedieu.cole@hotmail.com>
2020-11-30 15:24:35 +0100

    Added funktions and new classes

--- app/src/main/java/data/UserDao.kt
+++ app/src/main/java/data/UserDao.kt
@@ -1,4 +1,18 @@
 package data
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import java.net.UnknownServiceException
+
+@Dao
 interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser (user:User)
+
+    @Query("SELECT * FROM user_table ORDER BY id ASC")
+    fun alldata(): LiveData <List<User>>
+
 }
