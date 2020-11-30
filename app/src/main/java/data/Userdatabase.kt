27c20f9e8637fd163ac41cd454473ade517ced8a
wedieu <wedieu.cole@hotmail.com>
2020-11-30 16:17:26 +0100

    Added funktions and new classes

--- app/src/main/java/data/Userdatabase.kt
+++ app/src/main/java/data/Userdatabase.kt
@@ -1,4 +1,39 @@
 package data
 
-class Userdatabase {
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class Userdatabase:RoomDatabase() {
+    abstract class UserDatabase: RoomDatabase (){
+
+
+
+        abstract fun userDaO(): UserDao
+
+        companion object {
+            @Volatile
+            private var INSTANCE: Userdatabase? = null
+
+            fun getDatabase(context: Context):Userdatabase{
+             val tempInstance = INSTANCE
+              if(tempInstance != null){
+                  return  tempInstance
+              }
+
+              synchronized(this){
+                  val instance = Room.databaseBuilder(
+                      context.applicationContext,Userdatabase::class.java,
+                      "User_table"
+                  ).build()
+                  INSTANCE = instance
+                  return instance
+              }
+
+            }
+
+        }
+
+
+    }
 }
