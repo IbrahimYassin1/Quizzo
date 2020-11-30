012fb112d3ad00447f37d750595673075aff33d5
wedieu <wedieu.cole@hotmail.com>
2020-11-30 15:19:15 +0100

    UserDao.kt

--- app/src/main/java/data/User.kt
+++ app/src/main/java/data/User.kt
@@ -1,8 +1,10 @@
 package data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity (tableName = "User_table")
 data class User(
+    @PrimaryKey(autoGenerate = true)
     val id: Int
 )
